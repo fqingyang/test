78c3004a64f6ccabf41b2e5c80326d8a86c01571
whp <952640558@qq.com>
2021-01-13 11:09:16 +0800

    修改首页

---
+++ app/src/main/java/com/baqiinfo/githubtest/activity/DevActivity.java
@@ -0,0 +1,20 @@
+package com.baqiinfo.githubtest.activity;
+
+import android.os.Bundle;
+
+import com.baqiinfo.githubtest.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+/** 
+ * description: 开一个新分支,在新分支中修改,测试完通过后,合并到主分支
+ * author: fenqgingyang
+ * date: 2021/1/13 11:08 
+ */
+public class DevActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev);
+    }
+}

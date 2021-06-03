cade5474a8fe5f9dadfc8fdd00f083b8ec957dbe
whp <952640558@qq.com>
2021-06-03 17:12:53 +0800

    修改首页

--- app/src/main/java/com/baqiinfo/githubtest/MainActivity.java
+++ app/src/main/java/com/baqiinfo/githubtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.baqiinfo.githubtest;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Uri uridata = getIntent().getData();
+        if (uridata!=null) {
+            String id=uridata.getQueryParameter("param");
+            Toast.makeText(this, "唤起参数:"+id, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
